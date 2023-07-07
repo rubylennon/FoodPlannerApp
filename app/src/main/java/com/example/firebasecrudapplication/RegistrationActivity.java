8f800edb837cce2ae9e517218489e4d2b24f97eb
Ruby Lennon <x19128355@student.ncirl.ie>
2023-07-07 12:25:46 +0100

    fixed registration validation bug, fixed progress bar, added failure notifications

--- app/src/main/java/com/example/firebasecrudapplication/RegistrationActivity.java
+++ app/src/main/java/com/example/firebasecrudapplication/RegistrationActivity.java
@@ -1,11 +1,15 @@
 package com.example.firebasecrudapplication;
 
+//@REF 1 - GeeksForGeeks Tutorial - https://www.youtube.com/watch?v=-Gvpf8tXpbc
+//@REF 2 - https://firebase.google.com/docs/auth/android/password-auth
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -13,10 +17,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -40,6 +48,7 @@ public class RegistrationActivity extends AppCompatActivity {
         cnfPwdEdt = findViewById(R.id.idEdtCnfPwd);
         registerBtn = findViewById(R.id.idBtnRegister);
         loadingPB = findViewById(R.id.idPBLoading);
+        loadingPB.setVisibility(View.GONE);
         mAuth = FirebaseAuth.getInstance();
         LoginTV = findViewById(R.id.idTVLogin);
 
@@ -63,12 +72,13 @@ public class RegistrationActivity extends AppCompatActivity {
                 String pwd = pwdEdt.getText().toString();
                 String cnfPwd = cnfPwdEdt.getText().toString();
 
-                if(!pwd.equals(cnfPwd)){
-                    Toast.makeText(RegistrationActivity.this, "Passwords must match", Toast.LENGTH_SHORT).show();
-                } else if(TextUtils.isEmpty(userName) && TextUtils.isEmpty(pwd) && TextUtils.isEmpty(cnfPwd)){
+                if(TextUtils.isEmpty(userName) || TextUtils.isEmpty(pwd) || TextUtils.isEmpty(cnfPwd)){
+                    loadingPB.setVisibility(View.GONE);
                     Toast.makeText(RegistrationActivity.this, "Please add your credentials...", Toast.LENGTH_SHORT).show();
+                } else if(!pwd.equals(cnfPwd)){
+                    Toast.makeText(RegistrationActivity.this, "Passwords must match", Toast.LENGTH_SHORT).show();
                 } else{
-                    mAuth.createUserWithEmailAndPassword(userName,pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                     mAuth.createUserWithEmailAndPassword(userName,pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful()){
@@ -77,13 +87,27 @@ public class RegistrationActivity extends AppCompatActivity {
                                 Intent i = new Intent(RegistrationActivity.this, MainActivity.class);
                                 startActivity(i);
                                 finish();
-                            } else{
+                            } else {
+                                Log.w("createUserWithEmail:failure", task.getException());
+
                                 loadingPB.setVisibility(View.GONE);
-                                Toast.makeText(RegistrationActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+
+                                if(Objects.requireNonNull(task.getException()).toString().contains("The email address is badly formatted.")){
+                                    Toast.makeText(RegistrationActivity.this, "Registration failed. Please add valid email.", Toast.LENGTH_SHORT).show();
+                                } else if(Objects.requireNonNull(task.getException()).toString().contains("The given password is invalid. [ Password should be at least 6 characters ]")){
+                                    Toast.makeText(RegistrationActivity.this, "Registration failed. Password should be at least 6 characters.", Toast.LENGTH_SHORT).show();
+                                } else if(Objects.requireNonNull(task.getException()).toString().contains("The email address is already in use by another account")){
+                                    Toast.makeText(RegistrationActivity.this, "Registration failed. The email address is already in use by another account.", Toast.LENGTH_SHORT).show();
+                                } else if(Objects.requireNonNull(task.getException()).toString().contains("The given password is invalid. [ Password should be at least 6 characters ]")){
+                                    Toast.makeText(RegistrationActivity.this, "Registration failed. The email address is already in use by another account.", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(RegistrationActivity.this, "Registration failed. Please try again...", Toast.LENGTH_SHORT).show();
+                                }
                             }
                         }
                     });
                 }
+
             }
         });
     }
